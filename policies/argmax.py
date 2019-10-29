55b311d42de35f2629231947db87cbb521b7f16b
gary <igormolybog@gmail.com>
2019-10-29 13:48:19 -0700

    implemented epsilon-greedy exploration

--- policies/argmax.py
+++ policies/argmax.py
@@ -2,16 +2,35 @@ import numpy as np
 
 class ArgMax(object):
 
-    def __init__(self, q_function):
+    def __init__(self, q_function, *args, **kwargs):
         '''
             q_function - function that returns an np.array of size Nactions or
             a function that takes in the actions
         '''
+        super(ArgMax, self).__init__(*args, **kwargs)
         self.q_function = q_function
 
-    def get_action(self, obs):
-        q_function(obs) = q_obs
+    def get_action(self, obs, t=None):
+        return self._get_action(obs)
+
+    def _get_action(self, obs):
+        q_obs = q_function(obs)
         if isinstance(q_obs, np.ndarray):
             return np.argmax(q_function(obs))
         else:
             raise Exception("q_function did not return an array. continuous control is not implemented")
+
+class EpsilonGreedyAM(ArgMax):
+
+    def __init__(self, exploration_schedule, *args, **kwargs):
+        super(EpsilonGreedyAM, self).__init__( *args, **kwargs)
+        self.exploration = exploration_schedule
+
+    def get_action(self, obs, t):
+        eps = self.exploration.value(t)
+        if np.random.random() < eps:
+            return int(q_function.NUM_ACTIONS*np.random.random())
+        else:
+            return self._get_action(self, obs)
+
+    
